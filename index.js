6befb30e066d935b140a7a2823aeeb003fed76cb
Rafay Tahir <rafayatahir@fb.com>
2022-10-21 16:46:14 +0100

    hehe

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const path = require('path');
 const express = require("express")
 const app = express()
 
 app.get("/", (res,req)=> {
-    res.sendFile(path.join(__dirname, '/index.html'));
-    
+    res.sendFile(path.join(__dirname, '/index.html'));   
 })
 
 app.listen(80)

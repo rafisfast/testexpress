822ae17286e52d5e2ae2962e4bda7cdc6017d655
Rafay Tahir <rafayatahir@fb.com>
2022-10-21 15:50:51 +0100

    render

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const app = express()
 
-app.get("/", ()=> {
+app.get("/", (res,req)=> {
     console.log("yeah received a request, im cool")
+    req.render("hello")
 })
 
 app.listen(80)

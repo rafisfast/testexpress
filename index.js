8c903e43e7b1264334b1cbc2f1f7c47ee468f209
Rafay Tahir <rafayatahir@fb.com>
2022-10-21 16:48:10 +0100

    adding

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (res,req)=> {
-    res.sendFile(path.join(__dirname, '/index.html'));   
+    res.sendfile(path.join(__dirname, '/index.html'));   
 })
 
 app.listen(80)
